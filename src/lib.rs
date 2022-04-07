70cb3e5ecafc76972cf7fe18fb7a65c123a5a434
Adin Schmahmann <adin.schmahmann@gmail.com>
2022-04-07 04:32:01 -0400

    almost there

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
-use std::{collections::BTreeMap, intrinsics::copy_nonoverlapping, convert::TryInto};
+use std::{collections::BTreeMap, intrinsics::{copy_nonoverlapping, transmute}, convert::TryInto, fs::File, io::Read, borrow::BorrowMut};
 
-use libipld::{codec::Codec, ipld, error::Error, Multihash, cid::CidGeneric};
+use libipld::{codec::Codec, ipld, error::Error, Multihash, cid::CidGeneric, Cid};
+
+mod bencode;
+mod wac;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+//#[cfg(feature = "wee_alloc")]
+//#[global_allocator]
+//static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 /// Allocate memory into the module's linear memory
 /// and return the offset to the start of the block.
@@ -82,6 +85,17 @@ unsafe fn pattern2(ptr: *mut u8, len: usize, func : fn(input: &[u8]) -> Result<V
 }
 
 fn decode_block(input: Vec<u8>, out_len : &mut u32) -> Vec<u8> {
+    let res = bencode::decode(input);
+    match res {
+        Ok(v) => {
+            *out_len = v.len() as u32;
+            return v;
+        }
+        Err(x) => panic!("{:#?}", x)
+    }
+}
+
+fn decode_block2(input: Vec<u8>, out_len : &mut u32) -> Vec<u8> {
         // A JSON deserializer. You can use any Serde Deserializer here.
         let mut deserializer = bt_bencode::Deserializer::from_slice(&input);
 
@@ -103,19 +117,60 @@ fn decode_block(input: Vec<u8>, out_len : &mut u32) -> Vec<u8> {
         return out
 }
 
-/*
-extern {
-    fn load_block(cid_bytes: *const u8, cid_length: u8, block_len : &mut u32) -> *const u8;
-}*/
+#[no_mangle]
+pub unsafe fn lbw() {
+    let dig1 : &[u8] = &[0x1 ; 20];
+    let mh1 = Multihash::wrap(0x11,dig1).unwrap();
+    let c = libipld::Cid::new_v1(0x55, mh1);
+    load_block_wrapper(c);
+}
 
-unsafe fn load_block(cid_bytes: *const u8, cid_length: u8, block_len : &mut u32) -> *const u8 {
-    let res = load_block_err(cid_bytes, cid_length, block_len);
+///*
+
+extern {
+    fn load_block(cid_bytes: *const u8, cid_length: u8) -> *const u8;
+}
+
+extern {
+    fn foo(info : u32) -> ();
+}
+
+unsafe fn load_block_wrapper(blk_cid : Cid) -> &'static [u8] {
+    let blk_cid_bytes = blk_cid.to_bytes();
+    let cidptr = blk_cid_bytes.as_ptr();
+
+    foo(42);
+    let blk_with_len_ptr = load_block(cidptr, blk_cid_bytes.len() as u8);
+    let block_len = *(blk_with_len_ptr as *const u64);
+
+    let blk_ptr = (blk_with_len_ptr as usize + 8) as *const u8;
+    let block_data = ::std::slice::from_raw_parts(
+        blk_ptr,
+        block_len as usize,
+    );
+    return block_data
+}
+//*/
+/*
+unsafe fn load_block_wrapper(blk_cid : Cid) -> &'static [u8] {
+    let blk_cid_bytes = blk_cid.to_bytes();
+    let cidptr = blk_cid_bytes.as_ptr();
+
+    let mut block_len = 0;
+    let res = load_block_err(cidptr, blk_cid_bytes.len() as u8, &mut block_len);
     match res {
         Err(_) => panic!("error"),
-        Ok(r) => r
+        Ok(blk_ptr) => {
+            let block_data = ::std::slice::from_raw_parts(
+                blk_ptr,
+                block_len as usize,
+            );
+            block_data
+        }
     }
 }
 
+*/
 unsafe fn load_block_err(cid_bytes: *const u8, cid_length: u8, block_len : &mut u32) -> Result<*const u8, Error> {
     let mut block_store = BTreeMap::new();
 
@@ -150,8 +205,16 @@ pub unsafe fn load_adl(ptr: *mut u8, len: usize) -> *const u8  {
 #[no_mangle]
 pub unsafe fn seek_adl(adlptr: *mut u8, offset : i64, whence : u32) -> u64  {
     let mut f : FileReader = std::ptr::read(adlptr as *const _);
-    
-    let mut new_offset : i64 = f.offset as i64;
+    let res = seek_adl_safe(&mut f, offset, whence);
+
+    foo(res as u32 + 2000000);
+    std::mem::forget(f);
+
+    return res
+}
+
+fn seek_adl_safe(f : &mut FileReader, offset : i64, whence : u32) -> u64  {
+    let mut new_offset : i64 = *f.offset as i64;
     match whence {
         0 => new_offset = offset,
         1 => new_offset += offset,
@@ -163,16 +226,30 @@ pub unsafe fn seek_adl(adlptr: *mut u8, offset : i64, whence : u32) -> u64  {
         panic!("offset cannot be less than 0")
     }
 
-    f.offset = new_offset as u64;
-    return f.offset;
+    *f.offset = new_offset as u64;
+    return *f.offset;
 }
 
 #[no_mangle]
-pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, buflen : u32) -> u32  {
+pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, bufleni : i32) -> u32  {
+    foo(1);
     let mut f : FileReader = std::ptr::read(adlptr as *const _) ;
+    let mut buf = Vec::from_raw_parts(bufptr, bufleni.try_into().unwrap(), bufleni.try_into().unwrap());
+    let res = read_adl_safer(&mut f, &mut buf);
+
+    std::mem::forget(f);
+    std::mem::forget(buf);
+
+    res
+}
+
+fn read_adl_safer(f : &mut FileReader, buf : &mut [u8]) -> u32 {
+    unsafe {foo(2);}
+    unsafe {foo(*f.offset as u32 + 100000);}
+    let buflen =buf.len() as u32;
 
     // skip if past the end
-    if f.offset >= f.length {
+    if *f.offset >= f.length {
         panic!("tried reading past the end of the file")
     }
 
@@ -181,7 +258,7 @@ pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, buflen : u32) -> u32  
     let mut bufrem = buflen;
 
     while at < f.length && bufrem > 0 {
-        if f.offset > at+f.piece_len {
+        if *f.offset > at+f.piece_len {
 			at += f.piece_len;
             piece_num += 1;
 			continue
@@ -190,37 +267,24 @@ pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, buflen : u32) -> u32  
         // fastforward the first one if needed.
         let blk_cid = f.pieces[piece_num];
         let block_data : &[u8];
-        let blk_cid_bytes = blk_cid.to_bytes();
 
-        match f.cached_blocks.get(&blk_cid) {
-            Some(blk) => {
-                block_data = blk
-            },
-            None => {
-                let cidptr = blk_cid_bytes.as_ptr();
-                let mut block_len : u32 = 0;
-                let blk_ptr = load_block(cidptr, blk_cid_bytes.len() as u8, &mut block_len);
-                
-                block_data = ::std::slice::from_raw_parts(
-                    blk_ptr,
-                    block_len as usize,
-                );
-            },
-        }
 
-		if at < f.offset {
-            let delta = (f.offset - at) as isize;
-            let block_start = block_data.as_ptr().offset(delta);
+        unsafe {foo(8);}
+        unsafe {block_data = load_block_wrapper(blk_cid);}
+        unsafe {foo(9);}
+
+		if at < *f.offset {
+            let delta = (*f.offset - at) as usize;
 
             let mut num_to_copy = bufrem;
             let block_rem = (block_data.len() - (delta as usize)) as u32;
             if num_to_copy > block_rem {
                 num_to_copy = block_rem;
             }
-            let numcpy = num_to_copy.try_into().unwrap();
+            let numcpy: usize = num_to_copy.try_into().unwrap();
 
-            let buf_offset = buflen - bufrem;
-            copy_nonoverlapping(block_start, bufptr.offset(buf_offset as isize), numcpy);
+            let buf_offset: usize = (buflen - bufrem).try_into().unwrap();
+            buf[(buf_offset as usize)..(buf_offset + numcpy)].copy_from_slice(&block_data[(delta as usize)..(delta+numcpy)]);
             bufrem-=numcpy as u32;
             at += numcpy as u64;
 		} else {
@@ -229,10 +293,10 @@ pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, buflen : u32) -> u32  
             if num_to_copy > block_rem {
                 num_to_copy = block_rem;
             }
-            let numcpy = num_to_copy.try_into().unwrap();
+            let numcpy = num_to_copy;
 
             let buf_offset = buflen - bufrem;
-            copy_nonoverlapping(block_data.as_ptr(), bufptr.offset(buf_offset as isize), numcpy);
+            buf[(buf_offset as usize)..((buf_offset + numcpy) as usize)].copy_from_slice(&block_data[0..numcpy as usize]);
             bufrem-=numcpy as u32;
             at += numcpy as u64;
         }
@@ -241,50 +305,54 @@ pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, buflen : u32) -> u32  
     }
 
     let num_read = buflen - bufrem;
-    f.offset += num_read as u64;
+    unsafe {foo(100+buflen);}
+    unsafe {foo(200+bufrem);}
+    *f.offset += num_read as u64;
+    unsafe {foo(400000+*f.offset as u32);}
     return num_read;
 }
 
-fn ipld_try_map(i : ipld::Ipld) -> Result<BTreeMap<String, ipld::Ipld>, Error> {
+fn ipld_try_map(i : wac::Wac) -> Result<BTreeMap<Vec<u8>, wac::Wac>, Error> {
     match i {
-        ipld::Ipld::Map(val) => return Ok(val),
+        wac::Wac::Map(val) => return Ok(val),
         _ => return Err(libipld::error::Error::msg("not a map"))
     }
 }
 
-fn ipld_try_int(i : ipld::Ipld) -> Result<i128, Error> {
+fn ipld_try_int(i : wac::Wac) -> Result<i128, Error> {
     match i {
-        ipld::Ipld::Integer(val) => return Ok(val),
+        wac::Wac::Integer(val) => return Ok(val),
         _ => return Err(libipld::error::Error::msg("not an integer"))
     }
 }
 
-fn ipld_try_bytes(i : ipld::Ipld) -> Result<Vec<u8>, Error> {
+fn ipld_try_bytestring(i : wac::Wac) -> Result<Vec<u8>, Error> {
     match i {
-        ipld::Ipld::Bytes(val) => return Ok(val),
-        _ => return Err(libipld::error::Error::msg("not bytes"))
+        wac::Wac::String(val) => return Ok(val),
+        _ => return Err(libipld::error::Error::msg("not a bytestring"))
     }
 }
 
 fn load_adl_internal<'a>(input: &[u8]) -> Result<Vec<u8>, Error> {
-            // Assume node is DAG-CBOR
-            let node : libipld::ipld::Ipld = libipld::codec_impl::IpldCodec::DagCbor.decode(&input)?;
+            // Assume node is WAC
+            let node = wac::from_bytes(input)?;
+
             let node_map = ipld_try_map(node)?;
 
-            let pieces_node = node_map.get("pieces")
+            let pieces_node = node_map.get("pieces".as_bytes())
                 .ok_or(libipld::error::Error::msg("pieces not in node"))?;
-            let pieces_bytes = ipld_try_bytes(pieces_node.to_owned())?;
+            let pieces_bytes = ipld_try_bytestring(pieces_node.to_owned())?;
             
             const SHA1_HASH_LEN : usize = 20;
             if pieces_bytes.len() % SHA1_HASH_LEN != 0 {
                 return Err(libipld::error::Error::msg("pieces string is not a multiple of 20 bytes"));
             }
 
-            let piece_length_node = node_map.get("piece length")
+            let piece_length_node = node_map.get("piece length".as_bytes())
             .ok_or(libipld::error::Error::msg("piece length not in node"))?;
             let piece_length_int = ipld_try_int(piece_length_node.to_owned())?;
 
-            let length_node = node_map.get("length")
+            let length_node = node_map.get("length".as_bytes())
             .ok_or(libipld::error::Error::msg("length not in node"))?;
             let length_int = ipld_try_int(length_node.to_owned())?;
 
@@ -296,11 +364,11 @@ fn load_adl_internal<'a>(input: &[u8]) -> Result<Vec<u8>, Error> {
             }
 
             let f = FileReader { 
-                offset: 0, 
+                offset: &mut 0, 
                 length: length_int as u64, 
                 piece_len: piece_length_int as u64, 
-                cached_blocks: BTreeMap::new(), 
-                pieces: pieces 
+                //cached_blocks: BTreeMap::new(), 
+                pieces: pieces,
             };
 
             let f = std::mem::ManuallyDrop::new(f);
@@ -309,11 +377,12 @@ fn load_adl_internal<'a>(input: &[u8]) -> Result<Vec<u8>, Error> {
             return Ok(bytes.to_vec());
 }
 
+#[repr(C)]
 struct FileReader<'a> {
-    offset: u64,
+    offset: &'a mut u64,
     length: u64,
     piece_len: u64,
-    cached_blocks: BTreeMap<libipld::Cid, &'a [u8]>,
+    //cached_blocks: BTreeMap<libipld::Cid, &'a [u8]>,
     pieces: Vec<libipld::Cid>,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,44 @@ use std::convert::TryInto;
 use wasm_ipld::{myalloc, decode, load_adl, read_adl};
 
 fn main() {
-    // create a `Vec<u8>` as input
-    let input = "i50e";
-    // call the `alloc` function
-    let ptr = myalloc(input.len());
-    let mut output : Vec<u8>;
-    unsafe {
-        // copy the contents of `input`into the buffer
-        // returned by `alloc`
-        std::ptr::copy(input.as_ptr(), ptr, input.len());
-        // call the `array_sum` function with the pointer
-        // and the length of the array
-        let mut output_len: u32 = 0;
-        let res_start = decode(ptr, input.len(), &mut output_len);
-        
-        let ol_res = output_len.try_into().unwrap();
-        output = vec![0; ol_res];
-        std::ptr::copy(res_start, output.as_mut_ptr(), ol_res);
-    }
-    // print the result
-    println!("Result: {:#?}", output);
+    testFoo("i50e");
+    testFoo("6:length");
+    testFoo("l4:spami42ee");
+    testFoo("d3:bar4:spam3:fooi42ee");
 
-    let hex_cbor_file_root = "A3667069656365735828010101010101010101010101010101010101010102020202020202020202020202020202020202026C7069656365206C656E67746814666C656E6774681828";
+    let hex_cbor_file_root = "090406066c656e6774680392d51006046e616d6506094b6f616c612e6a7067060c7069656365206c656e677468038080100606706965636573062813da56fd10d288769fdea62d464572c5f16e967d4f02d20480f315ab16f71bf834a968e65baf90c5";
+    //let hex_cbor_file_root = "a4646e616d65694b6f616c612e6a7067666c656e6774681a00042a9266706965636573782813da56fd10d288769fdea62d464572c5f16e967d4f02d20480f315ab16f71bf834a968e65baf90c56c7069656365206c656e6774681a00040000";
+    //let hex_cbor_file_root = "A3667069656365735828010101010101010101010101010101010101010102020202020202020202020202020202020202026C7069656365206C656E67746814666C656E6774681828";
     let mut cbor_file_root = hex::decode(hex_cbor_file_root).unwrap();
 
     unsafe {
         let adl_ptr = load_adl(cbor_file_root.as_mut_ptr(), cbor_file_root.len());
         let mut buffer : Vec<u8> = Vec::new();
-        buffer.resize(100, 0);
+        buffer.resize(40, 0);
         
-        let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.capacity() as u32);
+        let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.capacity() as i32);
         println!("Result: read {:#?} bytes", num_read);
         println!("Result: read {:#?} ", buffer.as_slice());
     }
+}
+
+fn testFoo(input : &str) {
+        // call the `alloc` function
+        let ptr = myalloc(input.len());
+        let mut output : Vec<u8>;
+        unsafe {
+            // copy the contents of `input`into the buffer
+            // returned by `alloc`
+            std::ptr::copy(input.as_ptr(), ptr, input.len());
+            // call the `array_sum` function with the pointer
+            // and the length of the array
+            let mut output_len: u32 = 0;
+            let res_start = decode(ptr, input.len(), &mut output_len);
+            
+            let ol_res = output_len.try_into().unwrap();
+            output = vec![0; ol_res];
+            std::ptr::copy(res_start, output.as_mut_ptr(), ol_res);
+        }
+        // print the result
+        println!("Result: {:#?}", output);
 }
