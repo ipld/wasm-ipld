1ee4df05268157bbe455d94fcbdafafa4a41adde
Adin Schmahmann <adin.schmahmann@gmail.com>
2022-04-10 22:18:03 -0400

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{collections::BTreeMap, convert::TryInto};
 
-use libipld::{ error::Error, Multihash, cid::CidGeneric, Cid};
+use libipld::{cid::CidGeneric, error::Error, Cid, Multihash};
 
 mod bencode;
 mod wac;
@@ -21,169 +21,105 @@ pub fn myalloc(len: usize) -> *mut u8 {
 
 /// Given a pointer to the start of a byte array and
 /// its length, decode it into a standard IPLD codec representation
-/// (for now DAG-CBOR)
+/// (for now WAC)
 #[no_mangle]
-pub unsafe fn decode(ptr: *mut u8, len: usize, out_len : &mut u32) -> *const u8 {
-    pattern(ptr, len, out_len, decode_block)
-}
-
-unsafe fn pattern(ptr: *mut u8, len: usize, out_len : &mut u32, func : fn(input: Vec<u8>, out_len : &mut u32) -> Vec<u8>) -> *const u8 {
-    // create a `Vec<u8>` from the pointer and length
-    // here we could also use Rust's excellent FFI
-    // libraries to read a string, but for simplicity,
-    // we are using the same method as for plain byte arrays
+pub unsafe fn decode(ptr: *mut u8, len: usize, out_len: &mut u32) -> *const u8 {
     let data = Vec::from_raw_parts(ptr, len, len);
+    let result = decode_block(data, out_len);
 
-    let result = func(data, out_len);
-    let ptr = result.as_ptr();
-
-    // take ownership of the memory block where the result string
-    // is written and ensure its destructor is not
-    // called whe the object goes out of scope
-    // at the end of the function
-    std::mem::forget(result);
-    // return the pointer to the uppercase string
-    // so the runtime can read data from this offset
-    ptr
+    let bx = result.into_boxed_slice();
+    Box::into_raw(bx) as *const u8
 }
 
-fn decode_block(input: Vec<u8>, out_len : &mut u32) -> Vec<u8> {
+fn decode_block(input: Vec<u8>, out_len: &mut u32) -> Vec<u8> {
     let res = bencode::decode(input);
     match res {
         Ok(v) => {
             *out_len = v.len() as u32;
             return v;
         }
-        Err(x) => panic!("{:#?}", x)
+        Err(x) => panic!("{:#?}", x),
     }
 }
 
-fn decode_block2(input: Vec<u8>, out_len : &mut u32) -> Vec<u8> {
-        // A JSON deserializer. You can use any Serde Deserializer here.
-        let mut deserializer = bt_bencode::Deserializer::from_slice(&input);
-
-        // A compacted JSON serializer. You can use any Serde Serializer here.
-        let output = Vec::new();
-        let mut serializer =serde_ipld_dagcbor::Serializer::new(output);
-    
-        // Prints `{"a boolean":true,"an array":[3,2,1]}` to stdout.
-        // This line works with any self-describing Deserializer and any Serializer.
-        serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
-    
-        // read a Rust `String` from the byte array,
-        //let input_str = String::from_utf8(data).unwrap();
-        // transform the string to uppercase, then turn it into owned bytes
-        //let mut upper = input_str.as_bytes().to_owned();
-        let out = serializer.into_inner();
-        *out_len = out.len() as u32;
-
-        return out
-}
-
-#[no_mangle]
-pub unsafe fn lbw() {
-    let dig1 : &[u8] = &[0x1 ; 20];
-    let mh1 = Multihash::wrap(0x11,dig1).unwrap();
-    let c = libipld::Cid::new_v1(0x55, mh1);
-    load_block_wrapper(c);
-}
-
-///*
-
 #[cfg(target_arch = "wasm32")]
-extern {
+extern "C" {
     fn load_block(cid_bytes: *const u8, cid_length: u8) -> *const u8;
 }
 
 #[cfg(target_arch = "wasm32")]
-extern {
-    fn foo(info : u32) -> ();
+extern "C" {
+    fn log_marker(info: u32) -> ();
 }
 
 #[cfg(target_arch = "wasm32")]
-unsafe fn load_block_wrapper(blk_cid : Cid) -> Box<[u8]> {
+unsafe fn load_block_wrapper(blk_cid: Cid) -> Box<[u8]> {
     let blk_cid_bytes = blk_cid.to_bytes();
     let cidptr = blk_cid_bytes.as_ptr();
 
-    foo(42);
+    log_marker(42);
     let blk_with_len_ptr = load_block(cidptr, blk_cid_bytes.len() as u8);
     let block_len = *(blk_with_len_ptr as *const u64);
 
     let blk_ptr = (blk_with_len_ptr as usize + 8) as *const u8;
-    let block_data = ::std::slice::from_raw_parts(
-        blk_ptr,
-        block_len as usize,
-    );
+    let block_data = ::std::slice::from_raw_parts(blk_ptr, block_len as usize);
     return block_data.to_owned().into_boxed_slice();
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-unsafe fn foo(_info : u32) -> () {
+unsafe fn log_marker(_marker: u32) -> () {
     //println!("Foo: {:#?}", info);
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-unsafe fn load_block_wrapper(blk_cid : Cid) -> Box<[u8]> {
+unsafe fn load_block_wrapper(blk_cid: Cid) -> Box<[u8]> {
     let mut block_len = 0;
+    log_marker(42);
     let res = load_block_err(blk_cid, &mut block_len);
-    let z = match res {
+    match res {
         Err(_) => panic!("error"),
         Ok(blk_data) => {
             let x = blk_data.into_boxed_slice();
             x
         }
-    };
-    unsafe {foo(z[0].into());}
-    return z
-}
-
-unsafe fn read_cid(cid_bytes: *const u8, cid_length: u8) -> Result<Cid, libipld::cid::Error> {
-    let cid_vec = Vec::from_raw_parts(cid_bytes as *mut u8, cid_length as usize, cid_length as usize);
-    let cid_bytes: &[u8] = &cid_vec;
-    libipld::Cid::read_bytes(cid_bytes)
+    }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-fn load_block_err(c : Cid, block_len : &mut u32) -> Result<Vec<u8>, Error> {
+fn load_block_err(c: Cid, block_len: &mut u32) -> Result<Vec<u8>, Error> {
     let mut block_store = BTreeMap::new();
 
     let dig1 = hex::decode("38666b8ba500faa5c2406f4575d42a92379844c2")?;
-    let blk1 : &[u8] = &[0x61 ; 30];
-    let mh1 = Multihash::wrap(0x11,&dig1)?;
+    let blk1: &[u8] = &[0x61; 30];
+    let mh1 = Multihash::wrap(0x11, &dig1)?;
     block_store.insert(libipld::Cid::new_v1(0x55, mh1), blk1);
 
     let dig2 = hex::decode("45dfb79d668374f6578b3128746dce59b7a02e80")?;
-    let blk2 : &[u8] = &[0x62 ; 10];
-    let mh2 = Multihash::wrap(0x11,&dig2)?;
+    let blk2: &[u8] = &[0x62; 10];
+    let mh2 = Multihash::wrap(0x11, &dig2)?;
     block_store.insert(libipld::Cid::new_v1(0x55, mh2), blk2);
 
     let block_entry = block_store.get(&c).ok_or(Error::msg("not found"))?;
     let mut block_data = Vec::new();
     block_data.extend_from_slice(block_entry);
     *block_len = block_data.len() as u32;
-    //std::mem::forget(block_store);
     let res = Ok(block_data);
-    return res
+    return res;
 }
 
 #[no_mangle]
-pub unsafe fn load_adl(ptr: *mut u8, len: usize) -> *const u8  {
-    let block_data = ::std::slice::from_raw_parts(
-        ptr,
-        len,
-    );
+pub unsafe fn load_adl(ptr: *mut u8, len: usize) -> *const u8 {
+    let block_data = ::std::slice::from_raw_parts(ptr, len);
 
     let result_or_err = load_adl_internal(block_data);
     match result_or_err {
         Err(error) => panic!("{:?}", error),
-        Ok(val) => {
-            Box::into_raw(val) as *const u8
-        }
+        Ok(val) => Box::into_raw(val) as *const u8,
     }
 }
 
 #[no_mangle]
-pub unsafe fn seek_adl(adlptr: *mut u8, offset : i64, whence : u32) -> u64  {
+pub unsafe fn seek_adl(adlptr: *mut u8, offset: i64, whence: u32) -> u64 {
     let mut fb = Box::from_raw(adlptr as *mut FileReader);
     let res = seek_adl_safe(fb.as_mut(), offset, whence);
 
@@ -191,13 +127,13 @@ pub unsafe fn seek_adl(adlptr: *mut u8, offset : i64, whence : u32) -> u64  {
     res
 }
 
-fn seek_adl_safe(f : &mut FileReader, offset : i64, whence : u32) -> u64  {
-    let mut new_offset : i64 = f.offset as i64;
+fn seek_adl_safe(f: &mut FileReader, offset: i64, whence: u32) -> u64 {
+    let mut new_offset: i64 = f.offset as i64;
     match whence {
         0 => new_offset = offset,
         1 => new_offset += offset,
         2 => new_offset = (f.length as i64) + offset,
-        _ => panic!("unsupported whence")
+        _ => panic!("unsupported whence"),
     }
 
     if new_offset < 0 {
@@ -209,10 +145,14 @@ fn seek_adl_safe(f : &mut FileReader, offset : i64, whence : u32) -> u64  {
 }
 
 #[no_mangle]
-pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, bufleni : i32) -> u32  {
+pub unsafe fn read_adl(adlptr: *mut u8, bufptr: *mut u8, bufleni: i32) -> u32 {
     let mut fb = Box::from_raw(adlptr as *mut FileReader);
-    
-    let mut buf = Vec::from_raw_parts(bufptr, bufleni.try_into().unwrap(), bufleni.try_into().unwrap());
+
+    let mut buf = Vec::from_raw_parts(
+        bufptr,
+        bufleni.try_into().unwrap(),
+        bufleni.try_into().unwrap(),
+    );
     let res = read_adl_safer(fb.as_mut(), &mut buf);
 
     Box::leak(fb);
@@ -221,8 +161,8 @@ pub unsafe fn read_adl(adlptr: *mut u8, bufptr : *mut u8, bufleni : i32) -> u32 
     res
 }
 
-fn read_adl_safer(f : &mut FileReader, buf : &mut [u8]) -> u32 {
-    let buflen =buf.len() as u32;
+fn read_adl_safer(f: &mut FileReader, buf: &mut [u8]) -> u32 {
+    let buflen = buf.len() as u32;
 
     // skip if past the end
     if f.offset >= f.length {
@@ -234,19 +174,21 @@ fn read_adl_safer(f : &mut FileReader, buf : &mut [u8]) -> u32 {
     let mut bufrem = buflen;
 
     while at < f.length && bufrem > 0 {
-        if f.offset > at+f.piece_len {
-			at += f.piece_len;
+        if f.offset > at + f.piece_len {
+            at += f.piece_len;
             piece_num += 1;
-			continue
-		}
+            continue;
+        }
 
         // fastforward the first one if needed.
         let blk_cid = f.pieces[piece_num];
-        let block_data : Box<[u8]>;
+        let block_data: Box<[u8]>;
 
-        unsafe {block_data = load_block_wrapper(blk_cid);}
+        unsafe {
+            block_data = load_block_wrapper(blk_cid);
+        }
 
-		if at < f.offset {
+        if at < f.offset {
             let delta = (f.offset - at) as usize;
 
             let mut num_to_copy = bufrem;
@@ -257,10 +199,11 @@ fn read_adl_safer(f : &mut FileReader, buf : &mut [u8]) -> u32 {
             let numcpy: usize = num_to_copy.try_into().unwrap();
 
             let buf_offset: usize = (buflen - bufrem).try_into().unwrap();
-            buf[(buf_offset as usize)..(buf_offset + numcpy)].copy_from_slice(&block_data[(delta as usize)..(delta+numcpy)]);
-            bufrem-=numcpy as u32;
+            buf[(buf_offset as usize)..(buf_offset + numcpy)]
+                .copy_from_slice(&block_data[(delta as usize)..(delta + numcpy)]);
+            bufrem -= numcpy as u32;
             at = numcpy as u64 + f.offset;
-		} else {
+        } else {
             let mut num_to_copy = bufrem;
             let block_rem = block_data.len() as u32;
             if num_to_copy > block_rem {
@@ -269,8 +212,9 @@ fn read_adl_safer(f : &mut FileReader, buf : &mut [u8]) -> u32 {
             let numcpy = num_to_copy;
 
             let buf_offset = buflen - bufrem;
-            buf[(buf_offset as usize)..((buf_offset + numcpy) as usize)].copy_from_slice(&block_data[0..numcpy as usize]);
-            bufrem-=numcpy as u32;
+            buf[(buf_offset as usize)..((buf_offset + numcpy) as usize)]
+                .copy_from_slice(&block_data[0..numcpy as usize]);
+            bufrem -= numcpy as u32;
             at += numcpy as u64;
         }
 
@@ -282,67 +226,71 @@ fn read_adl_safer(f : &mut FileReader, buf : &mut [u8]) -> u32 {
     return num_read;
 }
 
-fn ipld_try_map(i : wac::Wac) -> Result<BTreeMap<Vec<u8>, wac::Wac>, Error> {
+fn ipld_try_map(i: wac::Wac) -> Result<BTreeMap<Vec<u8>, wac::Wac>, Error> {
     match i {
         wac::Wac::Map(val) => return Ok(val),
-        _ => return Err(libipld::error::Error::msg("not a map"))
+        _ => return Err(libipld::error::Error::msg("not a map")),
     }
 }
 
-fn ipld_try_int(i : wac::Wac) -> Result<i128, Error> {
+fn ipld_try_int(i: wac::Wac) -> Result<i128, Error> {
     match i {
         wac::Wac::Integer(val) => return Ok(val),
-        _ => return Err(libipld::error::Error::msg("not an integer"))
+        _ => return Err(libipld::error::Error::msg("not an integer")),
     }
 }
 
-fn ipld_try_bytestring(i : wac::Wac) -> Result<Vec<u8>, Error> {
+fn ipld_try_bytestring(i: wac::Wac) -> Result<Vec<u8>, Error> {
     match i {
         wac::Wac::String(val) => return Ok(val),
-        _ => return Err(libipld::error::Error::msg("not a bytestring"))
+        _ => return Err(libipld::error::Error::msg("not a bytestring")),
     }
 }
 
 fn load_adl_internal<'a>(input: &[u8]) -> Result<Box<FileReader>, Error> {
-            // Assume node is WAC
-            let node = wac::from_bytes(input)?;
+    // Assume node is WAC
+    let node = wac::from_bytes(input)?;
 
-            let node_map = ipld_try_map(node)?;
+    let node_map = ipld_try_map(node)?;
 
-            let pieces_node = node_map.get("pieces".as_bytes())
-                .ok_or(libipld::error::Error::msg("pieces not in node"))?;
-            let pieces_bytes = ipld_try_bytestring(pieces_node.to_owned())?;
-            
-            const SHA1_HASH_LEN : usize = 20;
-            if pieces_bytes.len() % SHA1_HASH_LEN != 0 {
-                return Err(libipld::error::Error::msg("pieces string is not a multiple of 20 bytes"));
-            }
+    let pieces_node = node_map
+        .get("pieces".as_bytes())
+        .ok_or(libipld::error::Error::msg("pieces not in node"))?;
+    let pieces_bytes = ipld_try_bytestring(pieces_node.to_owned())?;
 
-            let piece_length_node = node_map.get("piece length".as_bytes())
-            .ok_or(libipld::error::Error::msg("piece length not in node"))?;
-            let piece_length_int = ipld_try_int(piece_length_node.to_owned())?;
+    const SHA1_HASH_LEN: usize = 20;
+    if pieces_bytes.len() % SHA1_HASH_LEN != 0 {
+        return Err(libipld::error::Error::msg(
+            "pieces string is not a multiple of 20 bytes",
+        ));
+    }
 
-            let length_node = node_map.get("length".as_bytes())
-            .ok_or(libipld::error::Error::msg("length not in node"))?;
-            let length_int = ipld_try_int(length_node.to_owned())?;
+    let piece_length_node = node_map
+        .get("piece length".as_bytes())
+        .ok_or(libipld::error::Error::msg("piece length not in node"))?;
+    let piece_length_int = ipld_try_int(piece_length_node.to_owned())?;
 
-            let mut pieces: Vec<libipld::Cid> = Vec::new();
-            for c in pieces_bytes.chunks_exact(SHA1_HASH_LEN) {
-                let mh = Multihash::wrap(0x11, c)?;
-                let cid = CidGeneric::new_v1(0x55, mh);
-                pieces.push(cid);
-            }
+    let length_node = node_map
+        .get("length".as_bytes())
+        .ok_or(libipld::error::Error::msg("length not in node"))?;
+    let length_int = ipld_try_int(length_node.to_owned())?;
 
-            let f = FileReader { 
-                offset: 0, 
-                length: length_int as u64, 
-                piece_len: piece_length_int as u64, 
-                //cached_blocks: BTreeMap::new(), 
-                pieces: pieces,
-            };
+    let mut pieces: Vec<libipld::Cid> = Vec::new();
+    for c in pieces_bytes.chunks_exact(SHA1_HASH_LEN) {
+        let mh = Multihash::wrap(0x11, c)?;
+        let cid = CidGeneric::new_v1(0x55, mh);
+        pieces.push(cid);
+    }
 
-            let fb = Box::new(f);
-            return Ok(fb);
+    let f = FileReader {
+        offset: 0,
+        length: length_int as u64,
+        piece_len: piece_length_int as u64,
+        pieces: pieces,
+    };
+
+    let fb = Box::new(f);
+    return Ok(fb);
 }
 
 #[repr(C)]
@@ -350,13 +298,5 @@ struct FileReader {
     offset: u64,
     length: u64,
     piece_len: u64,
-    //cached_blocks: BTreeMap<libipld::Cid, &'a [u8]>,
     pieces: Vec<libipld::Cid>,
-}
-
-unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    ::std::slice::from_raw_parts(
-        (p as *const T) as *const u8,
-        ::std::mem::size_of::<T>(),
-    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,100 @@
-use std::convert::TryInto;
+#[cfg(test)]
+mod tests {
+    use std::convert::TryInto;
 
-use wasm_ipld::{myalloc, decode, load_adl, read_adl};
+    use wasm_ipld::{decode, load_adl, myalloc, read_adl, seek_adl};
 
-fn main() {
-    testFoo("i50e");
-    testFoo("6:length");
-    testFoo("l4:spami42ee");
-    testFoo("d3:bar4:spam3:fooi42ee");
-
-    // note: 09 means wac
-    let hex_cbor_file_root = "090406046e616d65060466696c65060c7069656365206c656e677468031e0606706965636573062838666b8ba500faa5c2406f4575d42a92379844c245dfb79d668374f6578b3128746dce59b7a02e8006066c656e6774680328";
-    //let hex_cbor_file_root = "090406066c656e6774680392d51006046e616d6506094b6f616c612e6a7067060c7069656365206c656e677468038080100606706965636573062813da56fd10d288769fdea62d464572c5f16e967d4f02d20480f315ab16f71bf834a968e65baf90c5";
-    //let hex_cbor_file_root = "a4646e616d65694b6f616c612e6a7067666c656e6774681a00042a9266706965636573782813da56fd10d288769fdea62d464572c5f16e967d4f02d20480f315ab16f71bf834a968e65baf90c56c7069656365206c656e6774681a00040000";
-    //let hex_cbor_file_root = "A3667069656365735828010101010101010101010101010101010101010102020202020202020202020202020202020202026C7069656365206C656E67746814666C656E6774681828";
-    let mut cbor_file_root = hex::decode(hex_cbor_file_root).unwrap();
-
-    unsafe {
-        let adl_ptr = load_adl(cbor_file_root.as_mut_ptr(), cbor_file_root.len());
-        let mut buffer : Vec<u8> = Vec::new();
-        buffer.resize(40, 0);
-        
-        let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
-        println!("Result: read {:#?} bytes", num_read);
-        println!("Result: read {:#?} ", buffer.as_slice());
-
-        let adl_ptr = load_adl(cbor_file_root.as_mut_ptr(), cbor_file_root.len());
-        let mut buffer : Vec<u8> = Vec::new();
-        buffer.resize(20, 0);
-
-        let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
-        println!("Result: read {:#?} bytes", num_read);
-        println!("Result: read {:#?} ", buffer.as_slice());
-
-        let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
-        println!("Result: read {:#?} bytes", num_read);
-        println!("Result: read {:#?} ", buffer.as_slice());
+    #[test]
+    fn test_int() {
+        assert_eq!(decode_string("i50e"), [3, 50])
     }
-}
 
-fn testFoo(input : &str) {
+    #[test]
+    fn test_string() {
+        assert_eq!(
+            decode_string("6:length"),
+            [6, 6, 108, 101, 110, 103, 116, 104]
+        )
+    }
+
+    #[test]
+    fn test_list() {
+        assert_eq!(
+            decode_string("l4:spami42ee"),
+            [8, 2, 6, 4, 115, 112, 97, 109, 3, 42]
+        )
+    }
+
+    #[test]
+    fn test_map() {
+        assert_eq!(
+            decode_string("d3:bar4:spam3:fooi42ee"),
+            [9, 2, 6, 3, 98, 97, 114, 6, 4, 115, 112, 97, 109, 6, 3, 102, 111, 111, 3, 42,]
+        )
+    }
+
+    #[test]
+    fn test_full_read() {
+        let hex_wac_file_root = "090406046e616d65060466696c65060c7069656365206c656e677468031e0606706965636573062838666b8ba500faa5c2406f4575d42a92379844c245dfb79d668374f6578b3128746dce59b7a02e8006066c656e6774680328";
+        let mut wac_file_root = hex::decode(hex_wac_file_root).unwrap();
+
+        unsafe {
+            let adl_ptr = load_adl(wac_file_root.as_mut_ptr(), wac_file_root.len());
+            let mut buffer: Vec<u8> = Vec::new();
+            buffer.resize(40, 0);
+
+            let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
+            assert_eq!(num_read, 40);
+            assert_eq!(buffer.as_slice()[0..30], [0x61; 30]);
+            assert_eq!(buffer.as_slice()[30..], [0x62; 10]);
+        }
+    }
+
+    #[test]
+    fn test_partial_reads() {
+        let hex_wac_file_root = "090406046e616d65060466696c65060c7069656365206c656e677468031e0606706965636573062838666b8ba500faa5c2406f4575d42a92379844c245dfb79d668374f6578b3128746dce59b7a02e8006066c656e6774680328";
+        let mut wac_file_root = hex::decode(hex_wac_file_root).unwrap();
+
+        unsafe {
+            let adl_ptr = load_adl(wac_file_root.as_mut_ptr(), wac_file_root.len());
+            let mut buffer: Vec<u8> = Vec::new();
+            buffer.resize(20, 0);
+
+            let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
+            assert_eq!(num_read, 20);
+            assert_eq!(buffer.as_slice()[0..20], [0x61; 20]);
+
+            let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
+            assert_eq!(num_read, 20);
+            assert_eq!(buffer.as_slice()[0..10], [0x61; 10]);
+            assert_eq!(buffer.as_slice()[10..], [0x62; 10]);
+        }
+    }
+
+    #[test]
+    fn test_seek_read() {
+        let hex_wac_file_root = "090406046e616d65060466696c65060c7069656365206c656e677468031e0606706965636573062838666b8ba500faa5c2406f4575d42a92379844c245dfb79d668374f6578b3128746dce59b7a02e8006066c656e6774680328";
+        let mut wac_file_root = hex::decode(hex_wac_file_root).unwrap();
+
+        unsafe {
+            let adl_ptr = load_adl(wac_file_root.as_mut_ptr(), wac_file_root.len());
+            let mut buffer: Vec<u8> = Vec::new();
+            buffer.resize(20, 0);
+
+            let seek_to = seek_adl(adl_ptr as *mut u8, 15, 0);
+            assert_eq!(seek_to, 15);
+
+            let num_read = read_adl(adl_ptr as *mut u8, buffer.as_mut_ptr(), buffer.len() as i32);
+            assert_eq!(num_read, 20);
+            assert_eq!(buffer.as_slice()[0..15], [0x61; 15]);
+            assert_eq!(buffer.as_slice()[15..], [0x62; 5]);
+        }
+    }
+
+    fn decode_string(input: &str) -> Vec<u8> {
         // call the `alloc` function
         let ptr = myalloc(input.len());
-        let mut output : Vec<u8>;
+        let mut output: Vec<u8>;
         unsafe {
             // copy the contents of `input`into the buffer
             // returned by `alloc`
@@ -50,11 +103,14 @@ fn testFoo(input : &str) {
             // and the length of the array
             let mut output_len: u32 = 0;
             let res_start = decode(ptr, input.len(), &mut output_len);
-            
+
             let ol_res = output_len.try_into().unwrap();
             output = vec![0; ol_res];
             std::ptr::copy(res_start, output.as_mut_ptr(), ol_res);
         }
-        // print the result
-        println!("Result: {:#?}", output);
+        println!("{:#?}", output);
+
+        return output;
+    }
 }
+fn main() {}
